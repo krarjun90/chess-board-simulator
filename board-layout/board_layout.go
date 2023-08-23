3047bef04b9458fe4e20eace709de16093cfc8ff
arjun.raveendra <arjun.raveendra@tesco.com>
2023-08-23 14:58:39 +0530

    parse square position

--- board-layout/board_layout.go
+++ board-layout/board_layout.go
@@ -1,17 +1,25 @@
 package board_layout
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
 )
 
-var size = 8
+const size = 8
 
-type boardLayout struct {
+var indexArr = []string{"A", "B", "C", "D", "E", "F", "G", "H"}
+
+type BoardLayout struct {
 	Data [][]string
 }
 
+type Cell struct {
+	Row int
+	Col int
+}
+
 func getRow(index int) []string {
-	indexArr := []string{"A", "B", "C", "D", "E", "F", "G", "H"}
 	var row []string
 	for i := 0; i < 8; i++ {
 		row = append(row, fmt.Sprintf("%v%v", indexArr[i], size-index))
@@ -19,8 +27,8 @@ func getRow(index int) []string {
 	return row
 }
 
-func NewBoardLayout() boardLayout {
-	b := boardLayout{}
+func NewBoardLayout() BoardLayout {
+	b := BoardLayout{}
 	b.Data = [][]string{}
 	for i := 0; i < size; i++ {
 		b.Data = append(b.Data, getRow(i))
@@ -28,7 +36,7 @@ func NewBoardLayout() boardLayout {
 	return b
 }
 
-func (b boardLayout) String() string {
+func (b BoardLayout) String() string {
 	str := ""
 	for i := 0; i < 8; i++ {
 		for j := 0; j < 8; j++ {
@@ -38,4 +46,30 @@ func (b boardLayout) String() string {
 		str += "\n"
 	}
 	return str
+}
+
+func ParseSquare(cellIndex string) (*Cell, error) {
+	if len(cellIndex) > 2 {
+		return nil, errors.New("more than 2 characters")
+	}
+	col := -1
+	for i, v := range indexArr {
+		if v == string(cellIndex[0]) {
+			col = i
+			break
+		}
+	}
+	if col == -1 {
+		return nil, errors.New("invalid column")
+	}
+
+	pos, err := strconv.ParseInt(string(cellIndex[1]), 10, 64)
+	if err != nil {
+		return nil, errors.New("invalid row")
+	}
+
+	if pos <= 0 || pos > 8 {
+		return nil, errors.New("invalid row")
+	}
+	return &Cell{Row: int(size - pos), Col: col}, nil
 }
